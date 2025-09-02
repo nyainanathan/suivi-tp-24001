ec6268d08607ad7d4ced4afe442f66a437ab10d6
Ny Aina Nathan <nyaina.nathan.1@gmail.com>
2025-09-02 15:11:09 +0300

    feat: implementation of exo 2

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import _pydatetime
 import base64
+import math
+import os
+import json
 from _pydatetime import datetime
 
 from fastapi.openapi.utils import status_code_ranges
@@ -15,3 +18,25 @@ app = FastAPI()
 @app.get("/ping")
 def ping():
     return Response(content="pong", status_code=200, media_type="text/plain")
+
+@app.get("/users")
+def get_users(Page: int , Size: int):
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    USERS_FILE = os.path.join(BASE_DIR, "users.json")
+
+    with open(USERS_FILE, "r", encoding="utf-8") as file:
+        users = json.load(file)
+
+    available_pages = math.ceil(len(users) / Size)
+
+    if(Page > available_pages) :
+        return JSONResponse(content={"error": "Bad types for provided query parameters"}, status_code=400)
+
+    required_users = []
+    start_index = Size * (Page - 1)
+    end_index = min(start_index + Size, len(users))
+
+    for i in range(start_index, end_index):
+        required_users.append(users[i])
+
+    return JSONResponse(content=required_users, status_code=200)
